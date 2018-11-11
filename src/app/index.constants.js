1d0760f7ccccd8bef6ded8c711159f4ca17be001
justformypractise <justformypractise@gmail.com>
2018-11-11 22:02:01 +0530

    inital commit

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('test1')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
